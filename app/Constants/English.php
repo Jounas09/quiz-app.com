3a30028662f2e6681b1d514d5a1e32f38121f7c8
miguel000127@gmail.com <miguel000127@gmail.com>
2024-07-22 17:47:58 -0500

    # create custom English lenguaje
    dsc: create a file to manage english languaje

---
+++ app/Constants/English.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Constants;
+
+class English
+{
+    /**
+     * Button Actions
+     */
+    const View_text = 'View';
+    const Take_text = 'Take';
+    const Configuration_text = 'Configurate';
+    const Update_text = 'Update';
+    const Delete_text = 'Delete';
+    const Save_text = 'Save';
+
+
+
+
+
+    /**
+     * Planifications
+     */
+    const Planification_text = 'Plans';
+
+
+    /**
+     * Errors
+     */
+    const Validation_text_error_for_planification = 'The planning date cannot be less than the current day.';
+    const Date_text_error_for_planification = 'The planning date cannot be less than the current day.';
+    const Type_text_error_for_planification = 'Please select a type.';
+    const Description_text_error_for_planification = 'Please enter a description.';
+    const Name_text_error_for_planification = 'Please enter a name.';
+
+
+}
