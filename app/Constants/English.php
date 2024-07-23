3e6f2266b7310608cc6fbf168fe46f15f60919b0
miguel000127@gmail.com <miguel000127@gmail.com>
2024-07-23 17:41:28 -0500

    # update constant file dsc: add constants to app

--- app/Constants/English.php
+++ app/Constants/English.php
@@ -13,15 +13,14 @@ class English
     const Update_text = 'Update';
     const Delete_text = 'Delete';
     const Save_text = 'Save';
-
-
-
-
+    const Add_text = 'Add';
+    const Select_text = 'Select an option';
 
     /**
      * Planifications
      */
     const Planification_text = 'Plans';
+    const Planification_text_default = 'No schedules available.';
 
 
     /**
@@ -32,6 +31,26 @@ class English
     const Type_text_error_for_planification = 'Please select a type.';
     const Description_text_error_for_planification = 'Please enter a description.';
     const Name_text_error_for_planification = 'Please enter a name.';
+
+    /***
+     * Banks
+     */
+    const Bank_title_text = 'Questions Bank';
+    const Bank_text = 'Bank';
+    const Questions_text = 'Questions';
+    const Configuations_text = 'Configurations';
+    const Answer_text = 'Answer';
+    const Enter_question_title= 'Enter a question title';
+    const Enter_answer_text= 'Enter an answer';
+    const Correct_answer_text = 'Select the correct answer';
+    const Select_course_text = 'Select the course';
+
+    /**
+     * Partials
+     */
+    const Test_title_text = 'Test';
+    const Task_title_text = 'Task';
+    const Class_title_text = 'Class';
 
 
 }
