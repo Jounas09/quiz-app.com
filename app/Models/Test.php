dc88aa26ba42d6b965b9af281444183c1648488f
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-03 15:35:17 -0500

    # create test logic
    dsc:
    - add model
    - add migration
    - add controller

---
+++ app/Models/Test.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tests';
+
+    protected $fillable = [
+        'id_Bank',
+        'question_number',
+        'duration_in_minutes'
+
+    ];
+
+    public function bank()
+    {
+        return $this->belongsTo(Banks::class, 'id_Bank');
+    }
+
+
+}
