bffde1b542bad740bd9497ce1dabb75a93f62373
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-05 22:57:01 -0500

    # add Spanish Languaje

--- app/Constants/English.php
+++ app/Constants/English.php
@@ -6,98 +6,204 @@ use App\Models\Planification;
 
 class English
 {
+
+
+    /**
+     * --------------------------------------------------------
+     *  English
+     * --------------------------------------------------------
+     */
+
     /**
      * Button Actions
      */
-    const View_text = 'View';
-    const Take_text = 'Take';
-    const Take_now_text = 'Take Now';
-    const Configuration_text = 'Configurate';
-    const Update_text = 'Update';
-    const Delete_text = 'Delete';
-    const Save_text = 'Save';
-    const Add_text = 'Add';
-    const Select_text = 'Select an option';
-    const Actions_text = 'Actions';
-    const Show_questions_text = 'Show Questions';
-    const Cancel_text = 'Cancel';
-    const Delete_modal_title = 'Do you want to delete';
-    const Next_text = 'Next';
-    const Back_text = 'Back';
-    const Teach_text = 'Teach';
-    const Sign_up_text = 'sign up';
+    // const View_text = 'View';
+    // const Take_text = 'Take';
+    // const Take_now_text = 'Take Now';
+    // const Configuration_text = 'Configurate';
+    // const Update_text = 'Update';
+    // const Delete_text = 'Delete';
+    // const Save_text = 'Save';
+    // const Add_text = 'Add';
+    // const Select_text = 'Select an option';
+    // const Actions_text = 'Actions';
+    // const Show_questions_text = 'Show Questions';
+    // const Cancel_text = 'Cancel';
+    // const Delete_modal_title = 'Do you want to delete';
+    // const Next_text = 'Next';
+    // const Back_text = 'Back';
+    // const Teach_text = 'Teach';
+    // const Sign_up_text = 'sign up';
 
-    /**
-     * Courses
+    // /**
+    //  * Courses
+    //  */
+
+    //  const Course_text = 'Course';
+    //  const Courses_text = 'Courses';
+    //  const Unaviable_courses_text = 'There are no courses available';
+
+    // /**
+    //  * Planifications
+    //  */
+    // const Planification_text = 'Plans';
+    // const Plan_text = 'To plan';
+    // const Planification_text_default = 'No schedules available.';
+    // const Planification_delete_modal_title = 'Do you want to delete';
+    // const Planification_delete_modal_error = 'Your planification have a question bank or info asociated';
+    // const Planification_delete_modal_success = 'Planification deleted';
+
+
+    // /**
+    //  * Errors
+    //  */
+    // const Validation_text_error_for_planification = 'The planning date cannot be less than the current day.';
+    // const Date_text_error_for_planification = 'The planning date cannot be less than the current day.';
+    // const Type_text_error_for_planification = 'Please select a type.';
+    // const Description_text_error_for_planification = 'Please enter a description.';
+    // const Name_text_error_for_planification = 'Please enter a name.';
+
+    // /***
+    //  * Banks
+    //  */
+    // const Bank_title_text = 'Questions Bank';
+    // const Bank_text = 'Bank';
+    // const Questions_text = 'Questions';
+    // const Configuations_text = 'Configurations';
+    // const Answer_text = 'Answer';
+    // const Enter_question_title= 'Enter a question title';
+    // const Enter_answer_text= 'Enter an answer';
+    // const Correct_answer_text = 'Select the correct answer';
+    // const Select_course_text = 'Select the course';
+
+
+    // /**
+    //  * Partials
+    //  */
+    // const Test_title_text = 'Test';
+    // const Task_title_text = 'Task';
+    // const Class_title_text = 'Class';
+
+    // /**
+    //  * Tests
+    //  */
+    // const Number_questions = 'Enter the number of questions';
+    // const Duration_test = 'Enter the duration of the test';
+    // const Total_questions_text = 'Total number of questions:';
+    // const Test_empty_text = 'No associated test found for this bank.';
+    // const Test_info_text = 'Test Info';
+    // const Test_questions_info_text = 'Questions number';
+    // const Test_duration_info_text = 'Duration';
+    // const Test_duration_mins_text = 'Duration in minutes';
+    // const Test_configuration_text = 'Configuration';
+
+    // /**
+    //  * Resposnes
+    // */
+
+    // const Confirmation_title_text = 'Do you really want to take the test';
+    // const Confirmation_body_text = 'If you press the OK button, the test will begin.';
+    // const Score_text = 'Score';
+    // const Response_text = 'Response';
+    // const Correct_Response_text = 'Correct response';
+    // const User_Response_text = 'User response';
+
+
+ /**
+     * --------------------------------------------------------
+     *  Español
+     * --------------------------------------------------------
      */
 
-     const Course_text = 'Course';
-     const Courses_text = 'Courses';
-     const Unaviable_courses_text = 'There are no courses available';
+     /**
+ * Acciones de Botón
+ */
+const View_text = 'Ver';
+const Take_text = 'Tomar';
+const Take_now_text = 'Tomar Ahora';
+const Configuration_text = 'Configurar';
+const Update_text = 'Actualizar';
+const Delete_text = 'Eliminar';
+const Save_text = 'Guardar';
+const Add_text = 'Agregar';
+const Select_text = 'Selecciona una opción';
+const Actions_text = 'Acciones';
+const Show_questions_text = 'Mostrar Preguntas';
+const Cancel_text = 'Cancelar';
+const Delete_modal_title = '¿Deseas eliminar?';
+const Next_text = 'Siguiente';
+const Back_text = 'Atrás';
+const Teach_text = 'Enseñar';
+const Sign_up_text = 'Registrarse';
 
-    /**
-     * Planifications
-     */
-    const Planification_text = 'Plans';
-    const Plan_text = 'To plan';
-    const Planification_text_default = 'No schedules available.';
-    const Planification_delete_modal_title = 'Do you want to delete';
-    const Planification_delete_modal_error = 'Your planification have a question bank or info asociated';
-    const Planification_delete_modal_success = 'Planification deleted';
+/**
+ * Cursos
+ */
+const Course_text = 'Curso';
+const Courses_text = 'Cursos';
+const Unaviable_courses_text = 'No hay cursos disponibles';
 
+/**
+ * Planificaciones
+ */
+const Planification_text = 'Planes';
+const Plan_text = 'Planificar';
+const Planification_text_default = 'No hay horarios disponibles.';
+const Planification_delete_modal_title = '¿Deseas eliminar?';
+const Planification_delete_modal_error = 'Tu planificación tiene un banco de preguntas o información asociada';
+const Planification_delete_modal_success = 'Planificación eliminada';
 
-    /**
-     * Errors
-     */
-    const Validation_text_error_for_planification = 'The planning date cannot be less than the current day.';
-    const Date_text_error_for_planification = 'The planning date cannot be less than the current day.';
-    const Type_text_error_for_planification = 'Please select a type.';
-    const Description_text_error_for_planification = 'Please enter a description.';
-    const Name_text_error_for_planification = 'Please enter a name.';
+/**
+ * Errores
+ */
+const Validation_text_error_for_planification = 'La fecha de planificación no puede ser menor que el día actual.';
+const Date_text_error_for_planification = 'La fecha de planificación no puede ser menor que el día actual.';
+const Type_text_error_for_planification = 'Por favor, selecciona un tipo.';
+const Description_text_error_for_planification = 'Por favor, ingresa una descripción.';
+const Name_text_error_for_planification = 'Por favor, ingresa un nombre.';
 
-    /***
-     * Banks
-     */
-    const Bank_title_text = 'Questions Bank';
-    const Bank_text = 'Bank';
-    const Questions_text = 'Questions';
-    const Configuations_text = 'Configurations';
-    const Answer_text = 'Answer';
-    const Enter_question_title= 'Enter a question title';
-    const Enter_answer_text= 'Enter an answer';
-    const Correct_answer_text = 'Select the correct answer';
-    const Select_course_text = 'Select the course';
+/***
+ * Bancos
+ */
+const Bank_title_text = 'Banco de Preguntas';
+const Bank_text = 'Banco';
+const Questions_text = 'Preguntas';
+const Configuations_text = 'Configuraciones';
+const Answer_text = 'Respuesta';
+const Enter_question_title= 'Ingresa un título para la pregunta';
+const Enter_answer_text= 'Ingresa una respuesta';
+const Correct_answer_text = 'Selecciona la respuesta correcta';
+const Select_course_text = 'Selecciona el curso';
 
+/**
+ * Parciales
+ */
+const Test_title_text = 'Prueba';
+const Task_title_text = 'Tarea';
+const Class_title_text = 'Clase';
 
-    /**
-     * Partials
-     */
-    const Test_title_text = 'Test';
-    const Task_title_text = 'Task';
-    const Class_title_text = 'Class';
+/**
+ * Pruebas
+ */
+const Number_questions = 'Ingresa el número de preguntas';
+const Duration_test = 'Ingresa la duración de la prueba';
+const Total_questions_text = 'Número total de preguntas:';
+const Test_empty_text = 'No se encontró ninguna prueba asociada a este banco.';
+const Test_info_text = 'Información de la Prueba';
+const Test_questions_info_text = 'Número de preguntas';
+const Test_duration_info_text = 'Duración';
+const Test_duration_mins_text = 'Duración en minutos';
+const Test_configuration_text = 'Configuración';
 
-    /**
-     * Tests
-     */
-    const Number_questions = 'Enter the number of questions';
-    const Duration_test = 'Enter the duration of the test';
-    const Total_questions_text = 'Total number of questions:';
-    const Test_empty_text = 'No associated test found for this bank.';
-    const Test_info_text = 'Test Info';
-    const Test_questions_info_text = 'Questions number';
-    const Test_duration_info_text = 'Duration';
-    const Test_duration_mins_text = 'Duration in minutes';
-    const Test_configuration_text = 'Configuration';
+/**
+ * Respuestas
+ */
+const Confirmation_title_text = '¿Realmente deseas tomar la prueba?';
+const Confirmation_body_text = 'Si presionas el botón OK, la prueba comenzará.';
+const Score_text = 'Puntuación';
+const Response_text = 'Respuesta';
+const Correct_Response_text = 'Respuesta correcta';
+const User_Response_text = 'Respuesta del usuario';
 
-    /**
-     * Resposnes
-    */
-
-    const Confirmation_title_text = 'Do you really want to take the test';
-    const Confirmation_body_text = 'If you press the OK button, the test will begin.';
-    const Score_text = 'Score';
-    const Response_text = 'Response';
-    const Correct_Response_text = 'Correct response';
-    const User_Response_text = 'User response';
 
 }
